f96331911e8b5a876bb2b7f96293df250f9c6d51
mzunohkaru <winkaisyu@gmail.com>
2024-07-13 17:52:44 +0900

    add:firebase init

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 ### Flutterのプロジェクトを作成
-`flutter create --org com.firebaseapp <Firebaseの本番環境のプロジェクトID("-"を"_"に置き換えたもの)>`
+`flutter create --org com.firebaseapp [Firebaseの本番環境のプロジェクトID("-"を"_"に置き換えたもの)]`
 
 ### Flavorizrを使って、Flutterのビルドを分ける
 `dart run flutter_flavorizr`
 
 ### エミュレータにビルド
 `flutter run --flavor dev -t lib/main_dev.dart`
+
+### Firebase Optionファイルの生成
+`flutterfire configure --out lib/gen/firebase_options_dev.dart --no-apply-gradle-plugins --platforms=android,ios --ios-bundle-id=[BUNDLE ID] --android-package-name=[ANDROID PACKAGE NAME]`
+`[BUNDLE ID] = com.firebaseapp.firSampleTool`
+`[ANDROID PACKAGE NAME] = com.firebaseapp.fir_sample_tool`
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'dart:async';
+import 'package:fir_sample/flavors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
+import 'gen/firebase_options_dev.dart' as dev;
+// import 'gen/firebase_options_prod.dart' as prod;
+
 FutureOr<void> main() async {
+  final flavor = F.appFlavor!;
+  final options = (flavor == Flavor.dev)
+      ? dev.DefaultFirebaseOptions.currentPlatform
+      : dev.DefaultFirebaseOptions
+          .currentPlatform; // prod.DefaultFirebaseOptions.currentPlatform
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: options,
+  );
   runApp(const App());
 }
