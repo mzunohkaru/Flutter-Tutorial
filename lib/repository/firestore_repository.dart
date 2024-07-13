880d41c6973dedb6190e8b7b54cad2e59b4114b6
mzunohkaru <winkaisyu@gmail.com>
2024-07-13 22:41:54 +0900

    feat:repository

---
+++ lib/repository/firestore_repository.dart
@@ -0,0 +1,27 @@
+import 'package:fir_sample/infrastructure/firestore/result.dart';
+
+import '../infrastructure/firestore/firestore_client.dart';
+import '../models/result/result.dart';
+import '../typedefs/firestore_typedef.dart';
+
+class FirestoreRepository {
+  FutureResult<bool> createDoc(DocRef ref, SDMap data) async {
+    final client = FirestoreClient();
+    try {
+      await client.createDoc(ref, data);
+      return const Result.success(true);
+    } catch (e) {
+      return const Result.failure();
+    }
+  }
+
+  FutureResult<Doc> getDoc(DocRef ref) async {
+    final client = FirestoreClient();
+    try {
+      final doc = await client.getDoc(ref);
+      return Result.success(doc);
+    } catch (e) {
+      return const Result.failure();
+    }
+  }
+}
